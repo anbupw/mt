21a8526d25d79438bb406499fe0d39c80699741b
Sweety <zoelanfikar@yahoo.co.id>
2024-11-17 03:21:36 +0800

    Add files via upload

---
+++ language/wuc_language.en.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * "Website Under Construction" PHP script from HTMLPIE.COM :)
+ * © HTMLPIE.COM . All rights reserved.
+ *
+ * @file
+ * The default language file.
+ *
+ * @version 2.1
+ *
+ * @name English
+ *
+ */
+
+  // Website title and description
+  define("WUC_L_SITE_TITLE",                     "Website Under Construction");
+  define("WUC_L_META_DESCRIPTION",               "");
+  define("WUC_L_PAGE_TITLE",                     "We are almost there !");
+
+  // Header slider.
+  define("WUC_L_SLIDE_1",                        "Hiya &amp; Welcome!");
+  define("WUC_L_SLIDE_2",                        "We are almost there!");
+  define("WUC_L_SLIDE_3",                        "Thanks for coming by!");
+
+  // Adding more is as easy as removing the double slash from the line below:
+
+  // define("WUC_L_SLIDE_4",                        "Lorem ipsum dolor");
+
+  // Please note, you should also use the constant WUC_L_SLIDE_4 in the index.php
+  // Find the WUC_L_SLIDE_1 in that file to see how.
+
+  // Language switch.
+  define("WUC_L_CHOOSE_LANGUAGE",                "Choose your language");
+
+  // Countdown heading.
+  define("WUC_L_COMING_SOON",                    "We will launch in exactly...");
+
+  // Progress bar heading, which is hidden by default.
+  define("WUC_L_PROGRESS",                       "Progress so far");
+
+  // Twitter block.
+  define("WUC_L_TWITTER",                        "Twitter");
+  define("WUC_L_FOLLOW",                         "Follow");
+
+  // For the static <noscript> version of the countdown.
+  define("WUC_L_DAYS",                           "days");
+
+  // Twitter "time ago" stuff.
+  define("WUC_L_RIGHT_NOW",                      "right now");
+  define("WUC_L_SECONDS_AGO",                    "%u seconds ago");
+  define("WUC_L_ABOUT_ONE_MINUTE_AGO",           "about 1 minute ago");
+  define("WUC_L_MINUTES_AGO",                    "%u minutes ago");
+  define("WUC_L_ABOUT_ONE_HOUR_AGO",             "about 1 hour ago");
+  define("WUC_L_HOURS_AGO",                      "%u hours ago");
+  define("WUC_L_YESTERDAY",                      "yesterday");
+  define("WUC_L_DAYS_AGO",                       "%u days ago");
+  define("WUC_L_OVER_A_YEAR_AGO",                "over a year ago");
+
+  // Contact us and subscribe forms.
+  define("WUC_L_SUBSCRIBE",                      "Join our invite list!");
+  define("WUC_L_CONTACT_US",                     "Leave a message");
+  define("WUC_L_REQUIRED",                       "Required");
+  define("WUC_L_REQUIRED_FIELDS",                "Required fields");
+  define("WUC_L_EMAIL",                          "E-mail");
+  define("WUC_L_YOUR_NAME",                      "Name");
+  define("WUC_L_YOUR_EMAIL",                     "E-mail address");
+  define("WUC_L_YOUR_MESSAGE",                   "Your Message");
+  define("WUC_L_SUBJECT_CONTACT_FORM",           "[Contact Form]");
+  define("WUC_L_MESSAGE",                        "Message");
+  define("WUC_L_SEND",                           "Send");
+  define("WUC_L_PLEASE_WAIT",                    "Please wait...");
+  define("WUC_L_EMAIL_INCORRECT",                "E-mail address is not correct.");
+  define("WUC_L_SOMETHING_WRONG",                "Something went wrong, please try again.");
+  define("WUC_L_ENTER_REQUIRED_FIELDS",          "Please fill all the required fields.");
+  define("WUC_L_WAIT_TWO_MINUTES",               "Please wait for two minutes then submit the form again.");
+  define("WUC_L_WAIT_THIRTY_MINUTES",            "Please wait for 30 minutes then submit the form again.");
+  define("WUC_L_WAIT_TWO_HOURS",                 "Please wait for 2 hours then submit the form again.");
+  define("WUC_L_CONTACT_THANKS",                 "Thanks for contacting us :)");
+  define("WUC_L_ALREADY_SUBSCRIBED",             "You have already subscribed :)");
+  define("WUC_L_SUBSCRIBE_THANKS",               "Thanks for subscribing! we will let you know once our website is up!");
+  define("WUC_L_SUBSCRIBE_AUTORESPONSE_SUBJECT", "Successfully subscribed to our newsletter");
+  define("WUC_L_SUBSCRIBE_AUTORESPONSE_BODY",    "Thank you for subscribing, we'll let you know once we went live!");
+  define("WUC_L_CONTACT_AUTORESPONSE_SUBJECT",   "We've received your message.");
+  define("WUC_L_CONTACT_AUTORESPONSE_BODY",      "We have received your message and will be in touch with you very soon, thanks for contacting us.");
+
+  // Footer message.
+  define("WUC_L_FOOTER_MESSAGE",                 "See you soon :)");
